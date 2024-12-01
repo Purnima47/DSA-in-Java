0b8935fcd656528171fc34806b3a82bf9fe85f68
Purnima Kesarwani <purnimakesarwani47@gmail.com>
2024-12-02 01:12:37 +0530

    Java files

--- 1. Beginner Level/2D Array/Diagonal Sum/DiagonalSum.java
+++ 1. Beginner Level/2D Array/Diagonal Sum/DiagonalSum.java
@@ -1,11 +1,15 @@
 // Leetcode Ques --> 1572 (Easy)
 public class DiagonalSum {
+    // Primary Diagonal --> i == j
+    // Secondary Diagonal ---> i + j = n - 1
     public static void main(String[] args) {
         int matrix[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
         int sum = 0;
         for (int i = 0; i < matrix.length; i++) {
+            // PD
             sum += matrix[i][i];
 
+            // SD
             if (i != matrix.length - 1 - i) {
                 sum += matrix[i][matrix.length - i - 1];
             }
