40d7fb92bf3fbee394a129be1f5836824342bbc1
Purnima Kesarwani <purnimakesarwani47@gmail.com>
2024-12-01 20:30:02 +0530

    Java files

--- 1. Beginner Level/Array + Array List/TrappingRainWater/TrappingRainwater.java
+++ 1. Beginner Level/Array + Array List/TrappingRainWater/TrappingRainwater.java
@@ -14,30 +14,30 @@ package TrappingRainWater;
  * 3. If we have bars in ascending or descending order of height then no water will be trapped.
  */
 
-//  waterLevel = minimum(maxLeftBoundary, maxRightBoundary)
+//  waterLevel = minimum(maxLeftBoundary, maxleft1Boundary)
 
 import java.util.*;
 
 public class TrappingRainwater {
     public static void trap(int height[], int n) {
-        // RIGHT MAX
-        int right[] = new int[n];
-        right[0] = height[0];
-        for (int i = 1; i < n; i++) {
-            right[i] = Math.max(right[i - 1], height[i]);
-        }
-
         // LEFT MAX
         int left[] = new int[n];
-        left[n - 1] = height[n - 1];
+        left[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            left[i] = Math.max(left[i - 1], height[i]);
+        }
+
+        // Right MAX
+        int right[] = new int[n];
+        right[n - 1] = height[n - 1];
         for (int i = n - 2; i >= 0; i--) {
-            left[i] = Math.max(height[i], left[i + 1]);
+            right[i] = Math.max(height[i], right[i + 1]);
         }
 
         // MIN OF BOTH
         int vol = 0;
         for (int i = 0; i < n; i++) {
-            vol += Math.min(right[i], left[i]) - height[i];
+            vol += Math.min(left[i], right[i]) - height[i];
         }
         System.out.println(vol);
     }
