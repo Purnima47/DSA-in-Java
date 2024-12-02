59533f44b5f10ea69161e1644587db533d49baf7
Purnima Kesarwani <purnimakesarwani47@gmail.com>
2024-12-02 18:48:26 +0530

    Java files

--- 2. Intermediate Level/Bit Manipulation + Hashing/UpdateIthBit.java
+++ 2. Intermediate Level/Bit Manipulation + Hashing/UpdateIthBit.java
@@ -9,12 +9,14 @@ public class UpdateIthBit {
         System.out.println(n | bitMask);
     }
 
+    // M - 1
     public static void updateithBit1(int n, int i, int newBit) {
         n = clearithBit(n, i);
         int bitMask = newBit << i;
         System.out.println(n | bitMask);
     }
 
+    // M - 2
     public static void updateithBit2(int n, int i, int newBit) {
         if (newBit == 0) {
             // clear ith bit
